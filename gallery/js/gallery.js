950be2ee89e2fbfa55aee4147f09a62c5c94684b
tenparsent <tenparsent@gmail.com>
2024-08-15 17:04:26 +0900

    Finish

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -5,15 +5,15 @@ function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
   let currentClass = "current";
-  //更新される画像情報
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
 
   thumbnails.forEach(function (thumbnail) {
+    //大画面をプリロードする
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function () {
       //クリックされたサムネイル画像をメイン画像として設定する
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
 
@@ -22,7 +22,11 @@ function activateGallery() {
       thumbnail.parentNode.classList.add(currentClass);
 
       // 画像の情報を更新する
-      title.innerHTML       = thumbnail.dataset.title;
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
   });
